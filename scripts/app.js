3e5f29643d3657e4153d4ec728f500d84be5f787
nefersillo <adrian.aguinaga@uabc.edu.mx>
2025-02-24 19:47:17 -0800

    106-2

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,20 @@
 
-function hello(){
-    console.log("Hello there");
+function saveTask(){
+    console.log("Saving task...");
+
+    
+
+
 }
 
 function init(){
     console.log("im the init fuction");
-    hello();
+    
+    //load data
+
+    //hook events
+    $("#btnSave").click(saveTask);
 }
 //new code change
 
-window.onload = hello;
+window.onload = init;
---
+++ scripts/task.js
@@ -0,0 +1,15 @@
+class Task{
+
+    constructor(title, description, color, startdate, status, budget)
+    {
+        this.title = title;
+        this.desc = description;
+        this.color = color;
+        this.startdate = startdate;
+        this.status = status;
+        this.budget = budget;
+        this.name = "Adrian";
+    }
+
+
+}
